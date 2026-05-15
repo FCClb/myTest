ca21b13be7809e36588ae905d917e74895900b21
Lenovo <123@.com>
2026-05-15 08:56:18 +0800

    spring cloud学习gateway - gateway 负载均衡

--- springcloud/04-gateway/src/main/java/com/fc/config/GatewayRoutesConfig.java
+++ springcloud/04-gateway/src/main/java/com/fc/config/GatewayRoutesConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 // 可以通过 yml 和Java配置类的方式来自定义配置路由规则
-@Configuration
+
+//已在yml中作了相同配置，暂时关闭此配置类
+//@Configuration
 public class GatewayRoutesConfig {
 
     @Bean
@@ -14,7 +16,9 @@ public class GatewayRoutesConfig {
         //1.基于routeLocatorBuilder获取构建route的Builder
         RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
         //2.设置route
-        return routes.route("path_route", route -> route.path("/order/**").uri("http://localhost:8090")).build();
+//        return routes.route("path_route", route -> route.path("/order/**").uri("http://localhost:8090")).build();
+        //负载均衡 ，到nacos中去找服务                                             修改uri为 lb://服务名
+        return routes.route("path_route", route -> route.path("/order/**").uri("lb://order")).build();
 
     }
 }
